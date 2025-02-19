6d3ab2884044e44f2f3e4543ba04a9a629db1d6a
Cody Nixon <cody.nixon@acuitybrands.com>
2025-02-19 11:26:42 -0700

    testing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello, Cody</h1>
+      <h2>This is a test of deployment.</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
