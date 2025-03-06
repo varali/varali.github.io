9cc410ff735f39a7159426380133344856bf2d90
Cody Nixon <cody.nixon@acuitybrands.com>
2025-03-06 08:26:30 -0700

    editing the site to be mine and not for the hackathon project

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Logo from "./components/Logo";
 import Info from "./components/Info";
 import Map from "./components/Map";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("service worker registered");
+  });
+}
+
 function App() {
-  const mapboxAccessToken =
-    "pk.eyJ1IjoiY3JuZXdib2xkIiwiYSI6ImNtN2NnYzZrYTBvcHEya3E4cWI0ajFtam0ifQ.macU8rj2--3ACPSKKlZ39g";
+  // const mapboxAccessToken =
+  //   "pk.eyJ1IjoiY3JuZXdib2xkIiwiYSI6ImNtN2NnYzZrYTBvcHEya3E4cWI0ajFtam0ifQ.macU8rj2--3ACPSKKlZ39g";
 
   return (
     <>
       <Logo />
-      <Map accessToken={mapboxAccessToken} />
+      {/* <Map accessToken={mapboxAccessToken} /> */}
       <Info />
     </>
   );
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,3 @@
 export default function Logo() {
-  return <h1>🗺️ BlueDot Precision 🎯</h1>;
+  return <h1>💜 Hello, Cody 💅</h1>;
 }
