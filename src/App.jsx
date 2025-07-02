703f1b0b717f9073d3ebcb51bec61c182ff51b7d
Cody Nixon <cody.nixon@acuitybrands.com>
2025-07-02 13:56:59 -0600

    adding more updates from react course, including homepage with Stoic quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Info from "./components/Info";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Schedule from "./pages/Schedule";
 import Journal from "./pages/Journal";
+import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function () {
@@ -12,20 +15,55 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+const BASE_URL = "http://localhost:8000";
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="schedule" element={<Schedule />} />
           <Route path="journal" element={<Journal />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-
-      <Info />
     </>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,42 +1,30 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 import PageNav from "../components/PageNav";
 
+const stoicQuotes = [
+  "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present. -Marcus Aurelius",
+  "Waste no more time arguing about what a good man should be. Be one. -Marcus Aurelius",
+  "I have to die. If it is now, well then I die now; if later, then now I will take my lunch, since the hour for lunch has arrived - and dying I will tend to later. -Epictetus",
+  "How much worse are the consequences of anger than that which caused it. -Marcus Aurelius",
+  "If someone is able to show me that what I think or do is not right, I will happily change, for I seek the truth, by which no one was ever truly harmed. It is the person who continues in his self-deception and ignorance who is harmed. -Marcus Aurelius",
+  "Until we have begun to go without them, we fail to realize how unnecessary many things are. We've been using them not because we needed them but because we had them. -Seneca",
+  "We have power over our minds, not outside events. Realize this and you will find strength. -Marcus Aurelius",
+  "If it is not right, do not do it, if it is not true, do not say it. -Marcus Aurelius",
+  "Wealth consists not in having great possessions but in having few wants. -Epictetus",
+  "We suffer more often in our imagination than reality. -Seneca",
+  "A man may rule the world and still be unhappy, if he does not feel that he is happy. What does your condition matter, if it is bad in your own eyes? -Seneca",
+  "Whatever is implanted by us when we were born can be train and toughened but not overcome. -Seneca",
+  "It is not about how long you live but how well you live. Life is long enough if you live it well. -Seneca",
+  "Death smiles at us all; all a man can do is smile back. -Marcus Aurelius",
+  "If you are distressed by anything external, the pain is not due to the thing itself but to your own estimate of it; and this you have the power to revoke at any moment. -Marcus Aurelius",
+  "Look well into thyself; there is a source of strength which will always spring up if thou wilt always look. -Marcus Aurelius",
+  "First say to yourself what you would be; and then do what you have to do. -Epictetus",
+  "The soul becomes dyed with the color of its thoughts. -Marcus Aurelius",
+  "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. -Marcus Aurelius",
+];
+
 export default function Homepage() {
-  const [affirmation, setAffirmation] = useState("Affirmation loading...");
-  useEffect(
-    function () {
-      async function getAffirmation() {
-        const url = "https://www.affirmations.dev";
-        // const url = "https://icanhazdadjoke.com/";
-        // const url = "https://zenquotes.io/api/today";
-        const options = {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Origin: "https://varali.github.io/",
-            "Access-Control-Allow-Origin": "https://varali.github.io/",
-          },
-          mode: "cors",
-        };
-
-        try {
-          const response = await fetch(url, options);
-          console.log(response);
-          const result = await response.text();
-          console.log(result);
-          setAffirmation(result["affirmation"]);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      getAffirmation();
-    },
-    [setAffirmation]
-  );
-
   return (
     <main className={styles.homepage}>
       <PageNav />
@@ -46,9 +34,9 @@ export default function Homepage() {
           <br />
           Welcome to your day.
         </h1>
-        <h2>{affirmation}</h2>
+        <h2>{stoicQuotes[Math.floor(Math.random() * stoicQuotes.length)]}</h2>
         <Link to="./app" className="cta">
-          Let's do it
+          Let&apos;s do it
         </Link>
       </section>
     </main>
