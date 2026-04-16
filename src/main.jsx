5590cd97625d5260d5dec55483584e74c828d6a2
Cody Newbold <15145703+varali@users.noreply.github.com>
2026-04-16 15:13:57 -0600

    creating a 404 redirection for known pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  const url = new URL(window.location.href);
+  url.searchParams.delete("redirect");
+  url.pathname = redirect;
+  window.history.replaceState(null, "", url.pathname + (url.search || "") + (url.hash || ""));
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
